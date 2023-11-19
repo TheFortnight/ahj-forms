c8167cacf9a0f5ef77704ad509e89467035c77a2
Александр Алябьев <aleksandr.b.aliabev@phrm.tech>
2023-11-19 20:19:39 +0300

    strange wp behavior

--- src/app.js
+++ src/app.js
@@ -1,2 +1,22 @@
-const Popup = require('./popup')
-const popup = new Popup('.button');
+//const Popup = require('./popup')
+//const popup = new Popup('.button');
+
+const subscribeWidget = document.querySelector('.subscribe');
+const subscribeForm = subscribeWidget.querySelector('.subscribe-form');
+const nameInput = subscribeWidget.querySelector('.name');
+const phoneInput = subscribeWidget.querySelector('.phone');
+
+subscribeForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState !== 4) return;
+        console.log('xhr.responseText: ' + xhr.responseText);
+    };
+
+    xhr.open('GET', 'http://localhost:8181');
+
+    xhr.send();
+});
