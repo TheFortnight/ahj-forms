6f9a6dfb0b48bbbd3f1997b61c5ab4a55662fdad
AplanaVM <alexander.alyabev@develonica.ru>
2023-11-21 23:08:23 +0300

    unsubs

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const subscribeWidget = document.querySelector('.subscribe');
 const subscribeForm = subscribeWidget.querySelector('.subscribe-form');
 const nameInput = subscribeWidget.querySelector('.name');
 const phoneInput = subscribeWidget.querySelector('.phone');
+const unsubscribeBtn = subscribeWidget.querySelector('.unsubscribe-btn');
 
 subscribeForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -19,7 +20,29 @@ subscribeForm.addEventListener('submit', (e) => {
         console.log('xhr.responseText: ' + xhr.responseText);
     };
 
-    xhr.open('GET', 'http://localhost:8181');
+    xhr.open('POST', 'http://localhost:8181');
+
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    xhr.send(body);
+});
+
+unsubscribeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const body = Array.from(subscribeForm.elements)
+        .filter(({ name }) => name)
+        .map(({ name, value}) => `${name}=${encodeURIComponent(value)}`)
+        .join('&');
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState !== 4) return;
+        console.log('xhr.responseText: ' + xhr.responseText);
+    };
+
+    xhr.open('DELETE', 'http://localhost:8181');
+
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
     xhr.send(body);
 });
